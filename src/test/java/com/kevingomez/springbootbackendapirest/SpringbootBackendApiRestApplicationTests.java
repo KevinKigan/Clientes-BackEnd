c9020247ba6ab191fd44451b14b446790a8727c9
Kevin Gómez <kevingomez.a98@gmail.com>
2020-05-05 15:58:46 +0200

    Initial commit

---
+++ src/test/java/com/kevingomez/springbootbackendapirest/SpringbootBackendApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevingomez.springbootbackendapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
