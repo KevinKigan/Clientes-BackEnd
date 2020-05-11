fbf9474b3cdf871ed332923b4810ce5fe4de544a
Kevin Gómez <kevingomez.a98@gmail.com>
2020-05-11 13:56:59 +0200

    Manejo de errores en metodo Handler show

--- src/main/java/com/kevingomez/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/kevingomez/springbootbackendapirest/controllers/ClienteRestController.java
@@ -3,10 +3,14 @@ package com.kevingomez.springbootbackendapirest.controllers;
 import com.kevingomez.springbootbackendapirest.models.DAO.Services.ClienteServiceInterface;
 import com.kevingomez.springbootbackendapirest.models.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -33,7 +37,23 @@ public class ClienteRestController {
      * @return Cliente cuyo ID es el buscado
      */
     @GetMapping("/clientes/{id}")
-    public Cliente show(@PathVariable int id){ return clienteService.findById(id); }
+    public ResponseEntity<?> show(@PathVariable int id){
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+        try{
+            cliente =clienteService.findById(id);
+        }catch (DataAccessException e){
+            response.put("mensaje", "Error al realizar la consulta en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if(cliente == null){
+            response.put("mensaje", "El cliente ID: ".concat(Integer.toString(id).concat(" no existe en la base de datos")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+    }
 
     /**
      * Metodo para crear un nuevo cliente
