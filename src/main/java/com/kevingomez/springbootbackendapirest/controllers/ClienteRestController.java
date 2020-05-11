7c0ab716a3006234a47ed5fe2fe3eaeb8461bf63
Kevin Gómez <kevingomez.a98@gmail.com>
2020-05-11 14:32:26 +0200

    Manejo de errores en metodo Handler update

--- src/main/java/com/kevingomez/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/kevingomez/springbootbackendapirest/controllers/ClienteRestController.java
@@ -84,18 +84,33 @@ public class ClienteRestController {
      * Metodo para actualizar un cliente
      *
      * @param id ID del cliente a actualizar
-     * @return Cliente actualizado
+     * @return ResponseEntity. Nos Permite pasar un mensaje de error y nuestro objeto entity a la respuesta
      */
     @PutMapping("/clientes/{id}")  // Se utiliza para actualizar un cliente
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente update(@RequestBody Cliente cliente, @PathVariable int id){
+    public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable int id){
         Cliente clienteActual = clienteService.findById(id);
+        Cliente clienteUpdated;
+        Map<String, Object> response = new HashMap<>();
 
-        clienteActual.setclientName (cliente.getclientName());
-        clienteActual.setEmail (cliente.getEmail());
-        clienteActual.setLastName (cliente.getLastName());
 
-        return clienteService.save(clienteActual);
+        if(clienteActual == null){
+            response.put("mensaje", "Error: no se puede editar, el cliente ID: ".concat(Integer.toString(id).concat(" no existe en la base de datos")));
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        try{
+            clienteActual.setclientName (cliente.getclientName());
+            clienteActual.setEmail (cliente.getEmail());
+            clienteActual.setLastName (cliente.getLastName());
+            clienteUpdated = clienteService.save(clienteActual);
+        }catch (DataAccessException e){
+            response.put("mensaje", "Error al realizar el insert en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("mensaje","El cliente ha sido actualizado con exito");
+        response.put("cliente", clienteUpdated);
+        return new ResponseEntity<Map>(response, HttpStatus.CREATED);
     }
 
     /**
