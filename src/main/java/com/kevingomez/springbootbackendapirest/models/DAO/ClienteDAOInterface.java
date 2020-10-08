910022f1481e1d157438de5aa64db26833916b6f
Kevin Gómez <kevingomez.a98@gmail.com>
2020-10-08 19:33:21 +0200

    Added Region Entity and Table Values
    Added Region Entity, the region of the client and Table Values

--- src/main/java/com/kevingomez/springbootbackendapirest/models/DAO/ClienteDAOInterface.java
+++ src/main/java/com/kevingomez/springbootbackendapirest/models/DAO/ClienteDAOInterface.java
@@ -1,8 +1,13 @@
 package com.kevingomez.springbootbackendapirest.models.DAO;
 
 import com.kevingomez.springbootbackendapirest.models.entity.Cliente;
+import com.kevingomez.springbootbackendapirest.models.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ClienteDAOInterface extends JpaRepository<Cliente, Integer> {
-
+    @Query("from Region") // No es el nombre de la tabla, sino el nombre de la clase Entity
+    List<Region> findAllRegions();
 }
